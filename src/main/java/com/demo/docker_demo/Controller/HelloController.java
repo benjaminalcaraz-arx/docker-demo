5a7c5cabdef0c1c267f72c209c6b7b60ab680a49
benjaminalcaraz-arx <benjamin.alcaraz@arxadia.fr>
2026-03-19 15:23:47 +0100

    Initial commit

---
+++ src/main/java/com/demo/docker_demo/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.docker_demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello depuis Docker ! 🐳";
+    }
+}
